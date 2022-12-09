994f8eec7996aa139b95ed9bd07168fd5807e2d4
maham000 <85379239+maham000@users.noreply.github.com>
2022-12-09 14:55:39 -0700

    Help finished with page navigation

--- YourYYC/LanguageOption.xaml.cs
+++ YourYYC/LanguageOption.xaml.cs
@@ -24,5 +24,50 @@ namespace YourYYC.Pages
         {
             InitializeComponent();
         }
+        public void HomeButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Home());
+        }
+        public void LanguageButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new LanguageOption());
+        }
+
+        public void HelpButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Help());
+        }
+        public void PreferencesButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Preferences());
+        }
+        public void AttractionsButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Attractions());
+        }
+        public void EventsButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());    // PLEASE ADD THIS 
+        }
+        public void FoodButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Food());
+        }
+        public void MapButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food()); // PLEASE ADD THIS
+        }
+        public void GettingThereButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new GettingThere());
+        }
+        public void ItineraryButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Itinerary1());
+        }
+        public void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.GoBack();
+        }
     }
 }
