433e6d43a4632248ad7f68f343efc71269689983
ethan m <ethanmacson@gmail.com>
2022-12-11 14:41:18 -0700

    city map added to master2

---
+++ YourYYC/Pages/CityMapSmall.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YourYYC.Pages
+{
+    /// <summary>
+    /// Interaction logic for CityMapMedium.xaml
+    /// </summary>
+    public partial class CityMapSmall : UserControl
+    {
+        public CityMapSmall()
+        {
+            InitializeComponent();
+        }
+        public void HomeButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Home());
+        }
+        public void LanguageButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());
+        }
+
+        public void HelpButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());
+        }
+        public void PreferencesButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Preferences());
+        }
+        public void AttractionsButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Attractions());
+        }
+        public void EventsButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());
+        }
+        public void FoodButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Food());
+        }
+        public void MapButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new CityMapLarge());
+        }
+        public void GettingThereButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());
+        }
+        public void ItineraryButtonClick(object sender, RoutedEventArgs e)
+        {
+            //Switcher.Switch(new Food());
+        }
+        public void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.GoBack();
+        }
+
+        private void DowntownPinClick(object sender, RoutedEventArgs e)
+        {
+            InglewoodSelected.Visibility = Visibility.Hidden;
+            PopUp.Visibility = Visibility.Visible;
+            DowntownSelected.Visibility = Visibility.Visible;
+        }
+
+        private void InglewoodPinClick(object sender, RoutedEventArgs e)
+        {
+            DowntownSelected.Visibility = Visibility.Hidden;
+            PopUp.Visibility = Visibility.Visible;
+            InglewoodSelected.Visibility = Visibility.Visible;
+        }
+
+
+        private void ZoomOutClick(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new CityMapMedium());
+        }
+
+        private void SideBarAttractionsClick(object sender, RoutedEventArgs e)
+        {
+            SideBarEventsTile.Visibility = Visibility.Collapsed;
+            SideBarFoodTile.Visibility = Visibility.Collapsed;
+        }
+
+        private void SideBarEventsClick(object sender, RoutedEventArgs e)
+        {
+            SideBarAttractionsTile.Visibility = Visibility.Collapsed;
+            SideBarFoodTile.Visibility = Visibility.Collapsed;
+        }
+
+        private void SideBarFoodClick(object sender, RoutedEventArgs e)
+        {
+            SideBarEventsTile.Visibility = Visibility.Collapsed;
+            SideBarAttractionsTile.Visibility = Visibility.Collapsed;
+        }
+    }
+}
